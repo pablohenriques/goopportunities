c77273a26acb7fdf238fbc187b49b5283713c3d7
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2024-10-20 17:21:38 -0300

    feat: organized handle module

--- router/routes.go
+++ router/routes.go
@@ -8,7 +8,7 @@ import (
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.GetOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
