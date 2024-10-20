93e38a888d637c8cc71a243dad969ebf4690b91a
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2024-10-20 18:39:27 -0300

    feat: schema and logs

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/pablohenriques/goopportunities/router"
+import (
+	"github.com/pablohenriques/goopportunities/config"
+	"github.com/pablohenriques/goopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initializar()
 }
